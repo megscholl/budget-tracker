4c8afd2a2459a36fb0cd0c68b031fbca093e6eca
Meg Scholl <meghan.e.scholl@gmail.com>
2018-05-05 12:56:25 -0500

    created js folder

---
+++ js/firebase.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+
+          ///////////////////////////////////////////////////
+          //////////// THIS JS FILE IS COMPLETE /////////////
+          ///////////////// REFERENCE ONLY //////////////////
+          ///////////////////////////////////////////////////
+
+
+function getFirebaseKey() {
+  return {
+    apiKey: "AIzaSyATHPzGdBWiE41Orek7tK-zeiW7GNESirU",
+    authDomain: "tend-income-tracker.firebaseapp.com",
+    databaseURL: "https://tend-income-tracker.firebaseio.com",
+    projectId: "tend-income-tracker",
+  };
+}
+
+module.exports = getFirebaseKey;
