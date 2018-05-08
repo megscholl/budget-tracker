28c9556a06bcafc32707d73a2001f8b836224f56
Meg Scholl <meghan.e.scholl@gmail.com>
2018-05-07 22:23:44 -0500

    started grunt, added gitignore file, when you click the save button it console logs

--- js/interaction.js
+++ js/interaction.js
@@ -1,3 +1,33 @@
 
+"use strict";
+
 let $ = require('../lib/node_modules/jquery'),
-    firebase = require('./firebase');
+    firebase = require('./firebasekey');
+
+
+    // budget object
+
+    $(document).on("click", "#save-btn", function() {
+        console.log("save button clicked");
+    });
+
+    function buildBudgetObject() {
+        let budgetObject = {
+            date: $("#date").val(),
+            job: $("#job").val(),
+            income: $("income").val()
+        };
+        return budgetObject;
+    }
+
+    //post budget to firebase
+
+    function addBudgetObj() {
+        return $.ajax({
+            url: `{firebase.getFBsettings().databaseURL}/income.json`,
+            type: 'POST',
+            data: JSON.stringify()
+        });
+    }
+
+    
